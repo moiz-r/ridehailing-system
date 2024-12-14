bc64d271224dc4f3a4312e1eaf6364a896a378eb
lazersocks <moiz.raza@careem.com>
2024-12-15 02:21:01 +0500

    user service done

---
+++ user-service/configs/config.go
@@ -0,0 +1,43 @@
+package configs
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+}
+
+type Config struct {
+	GRPCPort string         `mapstructure:"grpcPort"`
+	Database DatabaseConfig `mapstructure:"database"`
+}
+
+// LoadConfig reads configuration from a file or environment variables.
+func LoadConfig() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("configs/") // Path to look for the config file
+	viper.AutomaticEnv()
+
+	// Read the configuration
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+		return nil, err
+	}
+
+	// Map configuration to struct
+	var config Config
+	if err := viper.UnmarshalKey("config", &config); err != nil {
+		log.Fatalf("Error unmarshalling config: %v", err)
+		return nil, err
+	}
+
+	return &config, nil
+}
